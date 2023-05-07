2190b0f22fecce39883722bad82e38d0ee720966
grbcas <grbcas@gmail.com>
2023-05-07 19:08:51 +0300

    test coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,7 +9,7 @@ def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     with pytest.raises(IndexError):
         arrs.get([], 0, "test")
-
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
 
 def test_slice():
     # assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
@@ -17,3 +17,7 @@ def test_slice():
 
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 0, 0) == []
+    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
+
